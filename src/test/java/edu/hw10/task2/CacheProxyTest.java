6b2a444408abd87332eae71fd23229b036391b8b
Abobus1k <ginkinvaginkin@gmail.com>
2023-12-17 15:12:57 +0000

    add hw 10

--- src/test/java/edu/hw10/task2/CacheProxyTest.java
+++ src/test/java/edu/hw10/task2/CacheProxyTest.java
@@ -9,13 +9,13 @@ public class CacheProxyTest {
     @Test
     public void testCacheFileCreation() {
         FibCalculator calculator = new FibCalculatorImpl();
-        String cacheDirectory = "/home/mocklinux/IdeaProjects/HomeTasks/src/main/java/edu/hw10/task2/cache"; // Замените на реальный путь
+        String cacheDirectory = "src/test/java/edu/hw10/task2/cache";
         boolean persist = true;
 
         FibCalculator proxy = CacheProxy.create(calculator, FibCalculator.class, persist, cacheDirectory);
         proxy.fib(5);
 
-        String cacheFileName = cacheDirectory + "/cache.ser"; // Путь к файлу кеша
+        String cacheFileName = cacheDirectory + "/cache.ser";
         assertTrue(new File(cacheFileName).exists(), "Cache file not created");
     }
 }
